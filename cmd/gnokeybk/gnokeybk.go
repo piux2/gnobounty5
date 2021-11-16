178fd89a0b0914964c5ea006c42802f569bf99bd
piux2 <pupu@rays-mbp.attlocal.net>
2021-11-16 01:59:55 -0800

    keybkApp basic flow

--- cmd/gnokeybk/gnokeybk.go
+++ cmd/gnokeybk/gnokeybk.go
@@ -13,6 +13,7 @@ import (
 	"github.com/gnolang/gno/pkgs/crypto/bip39"
 	"github.com/gnolang/gno/pkgs/crypto/keys"
 	"github.com/gnolang/gno/pkgs/crypto/keys/client"
+	"github.com/gnolang/gno/pkgs/crypto"
 	"github.com/gnolang/gno/pkgs/errors"
 	//	"github.com/gnolang/gno/pkgs/sdk/vm"
 	//	"github.com/gnolang/gno/pkgs/std"
@@ -46,8 +47,8 @@ type localInfoBK struct {
 	PubKey  crypto.PubKey `json:"pubkey"` // backup key derived from ed25519
 	PrivKeyArmor string `json:"privkey.armor"` // privated key in armored ASCII format
 	//  A ED25519 signature that sign  the  ecoded JSON string  Name + Pubkey
-	Signature  []byte 'json:"signature"'
-	
+	Signature  []byte `json:"signature"`
+
 }
 
 // It finds the address to the key name and ask user to generate a new  priviate key with the same  nemonic
@@ -58,18 +59,24 @@ func backupKeyApp(cmd *command.Command, args []string, iopts interface{}) error 
 	opts := iopts.(client.BaseOptions)
 
 	if len(args) != 1 {
+
 		cmd.ErrPrintln("Usage: gnokeybk bkkey <keyname>")
 	}
 
-	// read primary key info
+	// read primary key's public info
 	name := args[0]
 	kb, err := keys.NewKeyBaseFromDir(opts.Home)
 	info, err := kb.Get(name)
-
 	if err != nil {
-		//TODO: call addApp to generate mnemonic and the primary key
-		return fmt.Errorf("%s does not exist. please add key first", name)
+                //TODO: call addApp to generate mnemonic and the primary key
+                return fmt.Errorf("%s does not exist. please add key first", name)
+        }
+	//TODO: add switch to support ledger info
+
+	if info.GetType() != keys.TypeLocal{
+		return errors.New("backup key only work for local private key")
 	}
+
 	addr := info.GetAddress()
 	cmd.Println(addr.String())
 	cmd.Println("This is your wallet address, please input corresponding mnemonic to generate back up key.")
@@ -85,15 +92,20 @@ func backupKeyApp(cmd *command.Command, args []string, iopts interface{}) error 
          // Maybe not, backup key already creates an  layer of security and extra burdon to the user. 
          // Plus, backward compatible  maintenaince will be a nightmare
 
-	encryptPassword, err := cmd.GetCheckPassword(
-			"Enter the passphrase to unlock the key store "
-		)
+	passphrase, err := cmd.GetPassword("Enter the passphrase to unlock the key store")
 
 	if err != nil {
 		return err
 	}
+	//var priv  crypto.PrivKey
+	//priv, err = kb.ExportPrivateKeyObject(name, passphrase)
 
-	kbBK := keys.NewLazyDBKeyBase("keybk", opts.Home)
+	if err != nil {
+
+		return fmt.Errorf("Please check the pass phrase for %s, it can not unlock the keybase.", name)
+	}
+
+	kbBK := keys.NewLazyDBKeybase("keybk", opts.Home)
 	bip39Message := "Enter your bip39 mnemonic"
 	mnemonic, err := cmd.GetString(bip39Message)
 
@@ -110,12 +122,24 @@ func backupKeyApp(cmd *command.Command, args []string, iopts interface{}) error 
 
 	}
 	// the bip39 passphrase is appendixed to the mnemonic to generate new account
-	account := 0
-	index := 0
-	info, err :=kb.CreateAccount(name, mnemonic, bip39Phassphrase, encryptPassword, account, index )
-	bkinfo, err := kbBK.CreateAccount(name, mnemonic, bipPhassphrase, encryptPassword, account, index)
+	//TODO: take care multi derived accounts from the same mnemonic
+	account := uint32(0)
+	index := uint32(0)
+
+	infobk, err := kbBK.CreateAccount(name, mnemonic, bip39Passphrase, passphrase, account, index)
+	// verify if mnemonic generate the same address
+	addrbk := infobk.GetAddress()
+	if addr.Compare(addrbk) !=0  {
+		mnemonicMsg := "The imput mnemonic is not correct.\n %s \n"
+		addrMsg:= "It does not match the address.\n %s \n" 
+		
+		return fmt.Errorf(
+			mnemonicMsg, mnemonic,  addrMsg , mnemonic,addr.String())
+
+	}
+	pubkeyBK := infobk.GetPubKey() 
+	cmd.Printfln("Backup pub key %s is created for address %s",pubkeyBK.String(), addrbk.String())
 	
-	// verify if mnemonic generate the same primary pubkey
-	
+
 	return nil
 }
